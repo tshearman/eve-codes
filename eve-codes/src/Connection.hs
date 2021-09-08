ba543f7fd771cb9c81a0d92f8786130e0ebcade6
Toby Shearman <toby@estimatingnature.com>
2021-09-07 23:16:35 -0400

    pass fix?

--- eve-codes/src/Connection.hs
+++ eve-codes/src/Connection.hs
@@ -5,7 +5,7 @@ import qualified Data.ByteString.Char8 as BC
 import Control.Retry
 
 connectionString :: String -> String -> String -> String -> String
-connectionString h user pass db = "host=" ++ h ++ " user=" ++ user ++ " password=\"" ++ pass ++"\" dbname=" ++ db
+connectionString h user pass db = "host=" ++ h ++ " user=" ++ user ++ " password=" ++ pass ++" dbname=" ++ db
 
 open :: String -> String -> String -> String -> IO PG.Connection
 open host user pass db = PG.connectPostgreSQL $ BC.pack (connectionString host user pass db)
